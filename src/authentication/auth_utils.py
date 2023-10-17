c8f761306417b9322c095c8c5bba172fe9da14fb
Vladimir Chernov <vecher.1@gmail.com>
2023-10-17 22:17:25 +0300

    docker-compose updated

--- src/authentication/auth_utils.py
+++ src/authentication/auth_utils.py
@@ -2,18 +2,21 @@ from datetime import datetime, timedelta
 from typing import Annotated
 import logging
 
-from fastapi import Depends, FastAPI, HTTPException, status, Query
+from fastapi import Depends, HTTPException, status, Query
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from pydantic import BaseModel
-import uvicorn
 
+from core.config import app_settings
+
+print('auth.py imported')
 # to get a string like this run:
 # openssl rand -hex 32
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = app_settings.SECRET_KEY
+
+ALGORITHM = app_settings.ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = app_settings.ACCESS_TOKEN_EXPIRE_MINUTES
 
 fake_users_db = {
     "johndoe": {
@@ -50,33 +53,41 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-app = FastAPI()
-
-
 def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
+    verification = pwd_context.verify(plain_password, hashed_password)
+    print(f'veify_password: {verification}')
+    return verification
 
 
 def get_password_hash(password):
-    return pwd_context.hash(password)
+    passwoed_hash = pwd_context.hash(password)
+    print(f'get_password_hash:\n   password = {password}\n   password_hash = {passwoed_hash}')
+    return passwoed_hash
 
 
 def get_user(db, username: str):
+    print(f'get user')
     if username in db:
         user_dict = db[username]
+        print(f'user: {username}')
         return UserInDB(**user_dict)
+    print(f'user is not found')
 
 
 def authenticate_user(fake_db, username: str, password: str):
+    print(f'authenticate_user')
     user = get_user(fake_db, username)
+    print(f'user: {user}')
     if not user:
         return False
     if not verify_password(password, user.hashed_password):
+        print(f'password is not veryfied')
         return False
     return user
 
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    print(f'create access token')
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
@@ -84,6 +95,7 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
         expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    print(f'access token: {encoded_jwt}')
     return encoded_jwt
 
 
@@ -94,11 +106,14 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
+        print(f'get_current_user: token: {token}')
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
+        print(f'decoded payload: {username}')
         if username is None:
             raise credentials_exception
         token_data = TokenData(username=username)
+        print(f'generated token: {token_data}')
     except JWTError:
         raise credentials_exception
     user = get_user(fake_users_db, username=token_data.username)
@@ -110,73 +125,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
 async def get_current_active_user(
         current_user: Annotated[User, Depends(get_current_user)]
 ):
+    print(f'get_current_active_user: {current_user.disabled}')
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
-
-
-@app.post("/login", response_model=Token)
-async def login_for_access_token(
-        form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
-):
-    user = authenticate_user(fake_users_db, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
-
-
-@app.get("/users/me/", response_model=User)
-async def read_users_me(
-        current_user: Annotated[User, Depends(get_current_active_user)]
-):
-    return current_user
-
-
-@app.get("/users/me/items/")
-async def read_own_items(
-        current_user: Annotated[User, Depends(get_current_active_user)]
-):
-    return [{"item_id": "Foo", "owner": current_user.username}]
-
-
-@app.get("/sign_up/", status_code=status.HTTP_201_CREATED)
-async def new_user(
-        user_name: Annotated[str, Query(min_length=4, max_length=64)],
-        password: Annotated[str, Query(min_length=4)]
-):
-    print(f'new user: {user_name}, {password}')
-    result: bool = False
-    try:
-        fake_users_db[user_name] = {
-            "username": user_name,
-            "hashed_password": pwd_context.hash(password),
-            "disabled": False
-        }
-        print(fake_users_db)
-        logging.info(f'user {user_name} added with password {password}')
-        print(f'hashed password: {fake_users_db[user_name]["hashed_password"]}')
-        result = True
-    except Exception as e:
-        logging.exception(f'Error: Sign-up:\n{e}')
-
-    if not result:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST
-        )
-    return {user_name: fake_users_db[user_name]["hashed_password"]}
-
-
-if __name__ == '__main__':
-    uvicorn.run(
-        'main:app',
-        host='127.0.0.1',
-        port=8400,
-    )
